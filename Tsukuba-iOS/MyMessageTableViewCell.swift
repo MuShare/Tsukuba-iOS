2ca9999468f1d5d2cc2586c6d748e3c7e14347d9
Meng Li <lm2343635@126.com>
2017-05-17 18:46:22 +0900

    Fix color issue in MyMessageTableViewCell.

--- Tsukuba-iOS/MyMessageTableViewCell.swift
+++ Tsukuba-iOS/MyMessageTableViewCell.swift
@@ -15,23 +15,14 @@ class MyMessageTableViewCell: UITableViewCell {
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var updateAtLabel: UILabel!
     
+    var closed = false
+    
     static let formatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
         formatter.timeStyle = .medium
         return formatter
     }()
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
     func fillWithMessage(_ message: Message) {
         self.titleLabel.text = message.title
@@ -44,16 +35,19 @@ class MyMessageTableViewCell: UITableViewCell {
     }
     
     func close() {
-        self.addSubview({
-            let view = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
-            view.backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 0.3, alpha: 0.5)
-            let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
-            label.text = NSLocalizedString("closed_message", comment: "")
-            label.textColor = .white
-            label.textAlignment = .center
-            view.addSubview(label)
-            return view
-        }())
+        if !closed {
+            self.addSubview({
+                let view = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
+                view.backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 0.3, alpha: 0.5)
+                let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
+                label.text = NSLocalizedString("closed_message", comment: "")
+                label.textColor = .white
+                label.textAlignment = .center
+                view.addSubview(label)
+                return view
+            }())
+            closed = true
+        }
     }
 
 }
