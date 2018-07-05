4b474b4797a61bdeb55ee2eb8292ea3b9326f00c
Meng Li <lm2343635@126.com>
2018-07-05 11:14:19 +0900

    Add RoomRowController.

--- Tsukuba-watchOS Extension/InterfaceController.swift
+++ Tsukuba-watchOS Extension/InterfaceController.swift
@@ -2,7 +2,7 @@
 //  InterfaceController.swift
 //  Tsukuba-watchOS Extension
 //
-//  Created by mon.ri on 2018/07/04.
+//  Created by Meng Li on 2018/07/04.
 //  Copyright © 2018 MuShare. All rights reserved.
 //
 
@@ -12,6 +12,8 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var roomTable: WKInterfaceTable!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -21,11 +23,19 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        roomTable.setNumberOfRows(1, withRowType: "roomCell")
+        let row = roomTable.rowController(at: 0) as! RoomRowController
+        row.nameLabel.setText("Test")
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
+        
     }
 
 }
---
+++ Tsukuba-watchOS Extension/RoomRowController.swift
@@ -0,0 +1,16 @@
+//
+//  RoomRowController.swift
+//  Tsukuba-watchOS Extension
+//
+//  Created by mon.ri on 2018/07/05.
+//  Copyright © 2018 MuShare. All rights reserved.
+//
+
+import WatchKit
+
+class RoomRowController: NSObject {
+
+    @IBOutlet var avatarImage: WKInterfaceImage!
+    @IBOutlet var nameLabel: WKInterfaceLabel!
+    
+}
