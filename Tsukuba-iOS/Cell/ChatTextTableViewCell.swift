1125ab83168c9175b964d93f6032bffa4bf7bba8
Meng Li <lm2343635@126.com>
2018-07-06 19:57:48 +0900

    Auto width for plain text message.

--- Tsukuba-iOS/Cell/ChatTextTableViewCell.swift
+++ Tsukuba-iOS/Cell/ChatTextTableViewCell.swift
@@ -23,49 +23,60 @@ class ChatTextTableViewCell: UITableViewCell {
         }
         
         struct message {
-            static let leadingMargin: CGFloat = 21
+            static let leadingMargin: CGFloat = 17
             static let trailingMargin: CGFloat = 14
-            static let topMargin: CGFloat = 4
-            static let bottomMargin: CGFloat = 12
+            static let topMargin: CGFloat = 6
+            static let bottomMargin: CGFloat = 10
+            static let adjustedValue: CGFloat = 12
         }
+        
+        static let maxMessageWidth = UIScreen.main.bounds.width - 2.0 * Const.backgroud.horizontalMargin
+            - Const.message.leadingMargin - Const.message.trailingMargin
     }
     
-    let maxMessageWidth = UIScreen.main.bounds.width - 2.0 * Const.backgroud.horizontalMargin
-        - Const.message.leadingMargin - Const.message.trailingMargin
-
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var messageTextView: UITextView!
     @IBOutlet weak var messageBackgroundImageView: UIImageView!
     
     func fill(with direction: ChatTextCellDirection, avatar: String, message: String) {
-        var messageWidth = maxMessageWidth
+        var messageWidth = Const.maxMessageWidth
         if let font = messageTextView.font {
-            messageWidth = message.width(with: font)
+            messageWidth = message.width(with: font) + Const.message.adjustedValue
         }
         switch direction {
         case .send:
-            messageBackgroundImageView.snp.makeConstraints {
-                $0.leadingMargin.equalTo(Const.backgroud.horizontalMargin + maxMessageWidth - min(messageWidth, maxMessageWidth))
+            messageBackgroundImageView.snp.updateConstraints {
+                $0.width.equalTo(min(messageWidth, Const.maxMessageWidth) + Const.message.leadingMargin + Const.message.trailingMargin)
                 $0.trailingMargin.equalTo(-Const.backgroud.horizontalMargin)
                 $0.topMargin.equalTo(Const.backgroud.topMargin)
                 $0.bottomMargin.equalTo(-Const.backgroud.bottomMargin)
             }
             
-            messageTextView.snp.makeConstraints {
+            messageTextView.snp.updateConstraints {
                 $0.left.equalTo(messageBackgroundImageView).offset(Const.message.trailingMargin)
                 $0.right.equalTo(messageBackgroundImageView).offset(-Const.message.leadingMargin)
                 $0.top.equalTo(messageBackgroundImageView).offset(Const.message.topMargin)
                 $0.bottom.equalTo(messageBackgroundImageView).offset(-Const.message.bottomMargin)
             }
         case .receive:
+            messageBackgroundImageView.snp.updateConstraints {
+                $0.width.equalTo(min(messageWidth, Const.maxMessageWidth) + Const.message.leadingMargin + Const.message.trailingMargin)
+                $0.leadingMargin.equalTo(Const.backgroud.horizontalMargin)
+                $0.topMargin.equalTo(Const.backgroud.topMargin)
+                $0.bottomMargin.equalTo(-Const.backgroud.bottomMargin)
+            }
             
+            messageTextView.snp.updateConstraints {
+                $0.left.equalTo(messageBackgroundImageView).offset(Const.message.leadingMargin)
+                $0.right.equalTo(messageBackgroundImageView).offset(-Const.message.trailingMargin)
+                $0.top.equalTo(messageBackgroundImageView).offset(Const.message.topMargin)
+                $0.bottom.equalTo(messageBackgroundImageView).offset(-Const.message.bottomMargin)
+            }
             break
         }
         
-        
         avatarImageView.kf.setImage(with: Config.shared.imageURL(avatar))
         messageTextView.text = message
-        
         
         let capInsets = UIEdgeInsetsMake(28, 20, 15, 20)
         switch reuseIdentifier {
