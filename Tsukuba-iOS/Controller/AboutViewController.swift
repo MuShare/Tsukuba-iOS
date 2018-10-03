2ea2c893edb20238e247859cc92d2a3dc4b3a416
Meng Li <lm2343635@126.com>
2018-10-03 16:00:55 +0900

    Use unowned self for the tap event closures.

--- Tsukuba-iOS/Controller/AboutViewController.swift
+++ Tsukuba-iOS/Controller/AboutViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    
+    deinit {
+        print("deinit \(type(of: self))")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
